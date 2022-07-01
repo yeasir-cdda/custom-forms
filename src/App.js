65fc693e9b954b068a9de6b61fb571ff655c1cdf
Yeasir <yeasir@cdda.io>
2022-07-02 02:01:03 +0600

    make appointment form done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import MakeAppointment from "./components/MakeAppointment";
+import { Box } from "@chakra-ui/react";
+import './font.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box width={'fit-content'} mx='auto' mt={'20'} >
+     <MakeAppointment/>
+    </Box>
   );
 }
 
---
+++ src/components/MakeAppointment.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Box, SimpleGrid,Button, Input, Textarea, Select, Text } from "@chakra-ui/react";
+const MakeAppointment = () => {
+
+    return (
+        <Box pt='40px' borderRadius={'10px'} boxShadow='md' width={'657px'} height={'579px'}>
+            <Box  textAlign={'center'}>
+                <Text fontWeight={'700'} letterSpacing='0.1px' color={'#252B42'} lineHeight='24px' >Contact Us</Text>
+                <Text mb={'40px'} fontWeight={'700'} fontSize='40px' lineHeight={'57px'} letterSpacing='0.2px' color={'#252B42'}>Make an Appointment</Text>
+                <Box width={'537px'} mx='auto' height='280px' >
+                    <form action="">
+                        <SimpleGrid columns={2} spacing='20px' >
+                            <Input fontSize={'14px'} color='#737373' lineHeight={'28px'} fontWeight='400' placeholder='Full Name*' />
+                            <Input fontSize={'14px'} color='#737373' lineHeight={'28px'} fontWeight='400' placeholder='Email*' />
+                            <Select fontSize={'14px'} color='#737373' lineHeight={'28px'} fontWeight='400' placeholder='Please Select'>
+                                <option value='option1'>Option 1 </option>
+                                <option value='option2'>Option 2 </option>
+                            </Select>
+                            <Select disabled fontSize={'14px'} color='#737373' lineHeight={'28px'} fontWeight='400' >
+                                <option value='4:00 Available'>4:00 Available </option>
+                                <option value='4:30 Available'>4:30 Available</option>
+                            </Select>
+                        </SimpleGrid>
+                        <Textarea mb='40px' mt={'20px'} height='140px' fontSize={'14px'} color='#252B42' lineHeight={'28px'} fontWeight='600' placeholder='Message' />
+                        <Button type='submit' fontSize={'14px'} width='224px' height={'58px'} _hover={{backgroundColor:'#1a90d3'}} fontWeight='700' color={'white'}  bg='#23A6F0' size='lg'>
+                        Book Appointment
+                        </Button>
+                    </form>
+                </Box>
+            </Box>
+        </Box>
+    );
+};
+
+export default MakeAppointment;
